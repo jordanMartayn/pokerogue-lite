4e6f3abe3967b481568013cc690d7cc8a163457a
jordanMartayn <jordanmartayn@mail.com>
2024-06-06 23:02:02 +0100

    working on screenshots of key elements

--- main.js
+++ main.js
@@ -1,106 +1,160 @@
-const { app, WebContentsView, BrowserWindow, ipcMain } = require('electron');
+//--Modules/Globals-------------------------------------------------------------------------------//
+const { app, WebContentsView, BrowserWindow, ipcMain, desktopCapturer, Menu, ipcRenderer } = require('electron');
 const path = require('node:path');
 const fs = require('fs');
 
 const devmode = true;
 let rogueView;
+let win;
+let rogueViewBoundsRequestDebounce = false;
 
 //--Utility Functions-----------------------------------------------------------------------------//
-const getPokerogueCanvasPixelData = () => {
-  return new Promise((resolve, reject) => {
-
-    //Try and extract pixel data from the pokerogue canvas 
-    (async () => {
-      try {
-        // Read the JavaScript code from getPokeRogueCanvas.js as string
-        const scriptPath = path.join(__dirname, 'scripts', 'getPokeRogueCanvas.js');
-        const getPokeRogueCanvas = fs.readFileSync(scriptPath, 'utf8');
-        const canvasJson = await rogueView.webContents.executeJavaScript(getPokeRogueCanvas);
-        resolve(canvasJson);
-  
-      } catch (err) {
-        console.error('Failed to execute JavaScript in webContents:', err);
-        reject(err);
-      }
-    })();
-
-  });
-}
-//--critical Functions----------------------------------------------------------------------------//
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 960,
-      height: 540,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js'),
-        nodeIntegration: true,
-        contextIsolation: false,
-        devTools: true,
-      }
-    });
-
-    if(devmode){
-      win.webContents.openDevTools();
-    }
-
-    //Setup the pokerogue view ontop of the main window as a percentage of the main window
-    rogueView = new WebContentsView({
-        preload: path.join(__dirname, 'preloadrogueView.js')
-    });
-    win.contentView.addChildView(rogueView)
-
-    const updateRogueViewBounds = () => {
-      const [mainWindowWidth, mainWindowHeight] = win.getContentSize();
-      const aspectRatio = mainWindowWidth / mainWindowHeight;
-      const desiredAspectRatio = 16 / 9;
-      let scale;
-      devmode ? scale = 0.5 : scale = 0.8; 
-      let width = 500;
-      let height = 1000;
-      /*
-      if(aspectRatio >= desiredAspectRatio){
+const updateRogueViewBounds = () => {
+    const [mainWindowWidth, mainWindowHeight] = win.getContentSize();
+    const aspectRatio = mainWindowWidth / mainWindowHeight;
+    const desiredAspectRatio = 16 / 9;
+    let scale;
+    devmode ? scale = 0.5 : scale = 0.8; 
+    let width = 500;
+    let height = 1000;
+    
+    if(aspectRatio >= desiredAspectRatio){
         //use height as the setting dimention
         height = Math.floor(mainWindowHeight * scale);
         //derive width from set height & desired aspect ratio 
-        width = Math.floor(desiredAspectRatio * height);        
-      }else{
+        width = Math.floor(desiredAspectRatio * height);                
+    }else{
         //use width as setting dimention etc...
         width = Math.floor(mainWindowWidth * scale);
         height = Math.floor(width / desiredAspectRatio);
-      }
-      */
-      rogueView.setBounds({
+    }
+    
+    const bounds = {
         x: 0, 
         y: 0, 
         width,
         height, 
-      })
     }
-    updateRogueViewBounds();
-    win.on('resize',updateRogueViewBounds);
-    rogueView.webContents.loadURL('https://pokerogue.net/')
-    //
-
-
-
-  
-    win.loadFile('index.html');
+    rogueView.setBounds(bounds);
+    win.webContents.send('updated-rogueview-bounds', bounds);
 }
 
+const rogueViewBoundsRequest = () => {
+    ipcMain.on('rogueview-bounds', (event) => {
+        if(rogueViewBoundsRequestDebounce) return;
+        rogueViewBoundsRequestDebounce = true;
+        setTimeout( () => {
+            rogueViewBoundsRequestDebounce = false;
+            const bounds = rogueView.getBounds();
+            event.reply('rogueview-bounds-res', bounds);
+        },500);
+    })
+}
+/*
+didnt work, cant get right buffer, keeping around incase i need to try and access the canvas again
+for other props
+const getPokerogueCanvasPixelData = () => {
+    return new Promise((resolve, reject) => {
+
+        //Try and extract pixel data from the pokerogue canvas 
+        (async () => {
+            try {
+                // Read the JavaScript code from getPokeRogueCanvas.js as string
+                const scriptPath = path.join(__dirname, 'scripts', 'getPokeRogueCanvas.js');
+                const getPokeRogueCanvas = fs.readFileSync(scriptPath, 'utf8');
+                const canvasJson = await rogueView.webContents.executeJavaScript(getPokeRogueCanvas);
+                resolve(canvasJson);
+    
+            } catch (err) {
+                console.error('Failed to execute JavaScript in webContents:', err);
+                reject(err);
+            }
+        })();
+
+    });
+}
+*/
+//--Critical Functions----------------------------------------------------------------------------//
+const createWindow = () => {
+        win = new BrowserWindow({
+            width: 960,
+            height: 540,
+            webPreferences: {
+                preload: path.join(__dirname, 'preload.js'),
+                nodeIntegration: true,
+                contextIsolation: false,
+                devTools: true,
+            },
+        });
+        win.setMenuBarVisibility(false);
+        win.loadFile('index.html');
+
+        //dev mode options
+        if(devmode){
+            win.webContents.openDevTools();
+        }
+
+        //Setup the pokerogue view ontop of the main window as a percentage of the main window
+        rogueView = new WebContentsView({
+                preload: path.join(__dirname, 'preloadrogueView.js'),
+        });
+        win.contentView.addChildView(rogueView)    
+        rogueView.webContents.loadURL('https://pokerogue.net/')
+        
+
+        //set size of pokerogue
+        setTimeout(updateRogueViewBounds,2000); //prevents a harmless border appearing on inital load
+        win.on('resize',updateRogueViewBounds);
+
+}
+
+//--Intialise-------------------------------------------------------------------------------------//
 app.whenReady().then(() => {
-    //initalise
-    createWindow();
+        createWindow();
 
-    //ipc Listeners
-    ipcMain.on('pixel-data', async (event) => {
-      const data = await getPokerogueCanvasPixelData();
-      event.reply( 'pixel-data-res', data );
-    });
+        //ipc Listeners
+        rogueViewBoundsRequest();
+        ipcMain.on('capture-rect', async (event, boundsInstruction) => {
+            //set bounds
+            let windowBounds;
 
-    //close
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') app.quit();
-    });
+            /* viewBounds sets bounds reletive to the pokerouge view as a decimal percentage,
+            i.e. if you want the a rectangle that takes the 20-40% of the width and 60-90& of the height
+            viewBounds = [0.2, 0.4, 0.6, 0.9]
+             */
+            let viewBounds = [xStartP, xEndP, yStartP, yEndP] = [0, 0, 0, 0];    
+
+            switch (boundsInstruction) {
+                case 'battle-pokemon-1-type':
+                    viewBounds = []
+                    break
+                default:
+                    bounds = rogueView.getBounds();
+            }
+
+            //get the pokerouge window and take a screen shot of bounds
+            const [winWidth, winHeight] = win.getSize();
+            const sources = await desktopCapturer.getSources({ 
+                types: ['window'], 
+                thumbnailSize: {
+                    width:winWidth,
+                    height:winHeight,
+                } 
+            });
+
+            for (const source of sources) {
+                if(source.name === 'pokerogue-lite') {
+                    const croppedImage = source.thumbnail.crop(windowBounds);
+                    const data = croppedImage.toPNG();
+                    fs.writeFileSync( path.join(__dirname, 'images', `${source.name}.png` ) , data);
+                }
+            }
+            
+            event.reply( 'capture-rect-res', 'image captured' );
+        });
+
+        //close
+        app.on('window-all-closed', () => {
+                if (process.platform !== 'darwin') app.quit();
+        });
 })
-
-//console.log('main.js - End');
--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,107 @@
+//--Modules/Globals-------------------------------------------------------------------------------//
 const { ipcRenderer } = require('electron');
 
+let rogueViewBounds;
+const LeftContent = document.getElementById('flexColLeft');
+const rightContent = document.getElementById('flexColRight');
+
 //--Util-Functions--------------------------------------------------------------------------------//
-const getPixelData = () => {
-    console.log('getting data');
-    ipcRenderer.send('pixel-data');
-    ipcRenderer.on('pixel-data-res', (event, res) => {
+const captureRect = bounds => {
+    /*bounds options:
+    default is the full pokerogue view
+
+    'battle-pokemon-1-type'
+    */
+    console.log('taking screenshot');
+    ipcRenderer.send('capture-rect', bounds);
+    ipcRenderer.on('capture-rect-res', (event, res) => {
         console.log(res);
     })
 }
+const addResizeUpdate = () => {
+    window.addEventListener('resize', () => {
+        ipcRenderer.send('rogueview-bounds');
+    })
+}
+const addCursorPosPercentageListener = () => {
+    const div = document.getElementById('cursorPosPercentageReadOut');
+    window.addEventListener('mousemove', (event) => {
+        const {clientX, clientY} = event;
+        const xPerc = (clientX / rogueViewBounds?.width).toFixed(2);
+        const yPerc = ( (clientY - rogueViewBounds?.height) / rogueViewBounds?.height ).toFixed(2);
+        div.innerText = 
+        `
+        xStart%: ${xPerc}
+        yStart%: ${yPerc}
+        `
+    })
+
+}
+const createDummyImage = () => {
+    let dummy = document.getElementById('dummyImage');
+    if(rogueViewBounds.width === 0 || rogueViewBounds.height === 0){
+        console.error("rogueVueBounds arn't 0");
+        return;
+    }
+    if(dummy) {
+        LeftContent.removeChild(dummy);
+        return;
+    }
+    dummy = document.createElement('img');
+    dummy.id = 'dummyImage';
+    dummy.src = './images/full.png';
+    dummy.style.width = rogueViewBounds.width +'px';
+    dummy.style.height = rogueViewBounds.height +'px'; 
+    LeftContent.appendChild(dummy);
+    addRectSizeToElem(dummy);
+}
+const addRectSizeToElem = (sadfsadfsadf) => {
+    console.log(sadfsadfsadf)
+    //TODO: cant seem to add a listener to it, image maybe, tried a delay, i was able to put
+    //one on a button via this method, next try to do it on a div not img.
+    
+    elem.addEventListener('click', (event) => {
+        //const {clientX, clientY} = event;
+        console.log('mousemove:',event)
+    })
+    console.log('not failed')
+    /*
+    elem.addEventListener('mouseup', (event) => {
+        //const {clientX, clientY} = event;
+        console.log('mouseDown:',event)
+    })
+    */
+}
 //--Critical-Functions----------------------------------------------------------------------------//
 const setupBtns = () => {
-    const pixelDataBtn = document.getElementById('getPixelDataBtn');
-    pixelDataBtn.addEventListener('click',getPixelData);
+    const pixelDataBtn = document.getElementById('captureRectBtn');
+    const bounds = 'battle-pokemon-1-type';
+    pixelDataBtn.addEventListener('click', () => {captureRect(bounds)} );
+
+    const dummyBtn = document.getElementById('dummyBtn');
+    dummyBtn.addEventListener('click', createDummyImage);
 }
+const utilityInitaliser = () => {
+    setTimeout( () => {
+        ipcRenderer.send('rogueview-bounds');
+    },2000)
+    
+    ipcRenderer.on('rogueview-bounds-res', (event, res) => {
+        rogueViewBounds = res;
+
+        const rogueviewSpacer = document.getElementById('rogueviewSpacer');
+
+        rogueviewSpacer.style.width = rogueViewBounds.width +'px';
+        rogueviewSpacer.style.height = rogueViewBounds.height +'px'; 
+        rogueviewSpacer.style.top = rogueViewBounds.height +'px'; 
+        LeftContent.style.width = rogueViewBounds.width +'px';
+    })
+} 
 
 //--Execute---------------------------------------------------------------------------------------//
 document.addEventListener('DOMContentLoaded', () => {
+    utilityInitaliser();
     setupBtns();
+    addResizeUpdate();
+    //addCursorPosPercentageListener();
 })
